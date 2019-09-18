e27695fffbf8c2e59bbd354f7f223c6deab723e4
Jesus Enrique Uribe Lopez <juribe@mosbit.com>
2019-09-18 13:49:42 -0500

    BASURA

--- src/componets/App.js
+++ src/componets/App.js
@@ -4,6 +4,7 @@ import Login from "../pages/Login/Login"
 import Home from "../pages/Home/Home"
 import isLoginHome from "../pages/isLoginHome/isLoginHome"
 import Reguistro from "../pages/Reguistro/Reguistro"
+import Emperesa from "../pages/Emperesa"
 
 
 const App =()=>(
@@ -12,6 +13,7 @@ const App =()=>(
         <Route exact path={'/Login'} component={Login}/>
         <Route exact path={'/isLoginHome'} component={isLoginHome}/>
         <Route exact path={'/Reguistro'} component={Reguistro}/>
+        <Route exact path={'/Emperesa'} component={Emperesa}/>
     </BrowserRouter>
 );
 export default App
---
+++ src/pages/Emperesa.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+
+class Emperesa extends Component {
+    render() {
+        return (
+            <div>
+                <h2>IOUIOU</h2>
+            </div>
+        );
+    }
+}
+
+export default Emperesa;
